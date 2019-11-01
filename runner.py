79eed47f34571ba3635e29ab55389410c9c7b535
Devon Wasson <devonwasson@gmail.com>
2019-11-01 14:08:21 +0000

    modified runner to not launch duplicate runners

--- runner.py
+++ runner.py
@@ -1,9 +1,6 @@
 import argparse
-import datetime
 import time
 import os
-
-minutes_running_too_long = 20
 
 parser = argparse.ArgumentParser()
 parser.add_argument('config_file_name', metavar='C', type=str)
@@ -13,26 +10,11 @@ subreddit_name = config_fname.split("-")[0]
 
 def main():
 	while True:
-		f = open('beingUsed/' + subreddit_name + '.txt', 'w')
-		f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
-		f.close()
 		os.system('python swap.py ' + config_fname)
 		time.sleep(30)
 
-f = open('beingUsed/' + subreddit_name + '.txt','r')
-beingUsed = f.read().strip()
-f.close()
-
-try:
-	delta_seconds = (datetime.datetime.now() - datetime.datetime.strptime(beingUsed, '%Y-%m-%d %H:%M:%S.%f')).total_seconds()
-except:
-	delta_seconds = 9999999999999
-
-if beingUsed == "":
-	main()
-elif (delta_seconds) > (60*minutes_running_too_long):
-	print("=====\nSeconds Delta: " + str(delta_seconds))
-	print("Previous Execution started at: " + beingUsed)
-	print("Current time is: " + str(datetime.datetime.now()) + "\n=====")
-	print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + " Running for too long, killing all and rerunning.")
+ps_output = os.popen('ps -ef | grep \&\&\ python\ runner.py\ ' + config_fname).read().splitlines()
+# If the only output we get from grep is the grep itself,
+# then runner is not currently running so this instance should take over
+if len(ps_output) == 1:
 	main()
