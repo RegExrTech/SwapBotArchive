a65426ed7322916e2a60bab582c5a04d413355d9
Devon Wasson <devonwasson@gmail.com>
2019-12-30 06:31:55 +0000

    using corrent api in add_comment now

--- add_comment.py
+++ add_comment.py
@@ -2,7 +2,7 @@ import requests
 
 request_url = "http://192.168.1.210:8000"
 
-id = ""
-sub = ""
+id = "fc42j0e"
+sub = "mousemarket"
 
-requests.post(request_url + "/add-swap/", {'sub_name': sub, 'comment_id': id})
+print(requests.post(request_url + "/add-comment/", {"sub_name": sub, "comment_id": id}))
