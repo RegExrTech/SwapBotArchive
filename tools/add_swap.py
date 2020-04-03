170aa7c9861f01dd10063c3bb2fe07981b6f974a
Devon Wasson <devonwasson@gmail.com>
2020-04-03 14:11:43 +0000

    updated request url for tools

--- tools/add_swap.py
+++ tools/add_swap.py
@@ -10,9 +10,8 @@ parser.add_argument('sub_name', metavar='C', type=str)
 parser.add_argument('username', metavar='C', type=str)
 #parser.add_argument('swap_text', metavar='C', type=str)
 args = parser.parse_args()
-request_url = "http://192.168.1.210:8000"
+request_url = "http://192.168.0.248:8000"
 #requests.post(request_url + "/add-swap/", {'sub_name': args.sub_name, 'username': args.username, 'V': args.swap_text})
 json_helper = JsonHelper()
 db = json_helper.get_db('database/swaps.json')
-for swap in db[args.sub_name]['lifeobenreilly']:
-	requests.post(request_url + "/add-swap/", {'sub_name': args.sub_name, 'username': args.username, 'swap_text': swap})
+requests.post(request_url + "/add-swap/", {'sub_name': args.sub_name.lower(), 'username': args.username.lower(), 'swap_text': "LEGACY TRADE"})
