58be8b9b92a91eba0eb8d1509be49c7515910885
Devon Wasson <devonwasson@gmail.com>
2020-05-20 18:50:47 +0000

    remove comment now suppots multi removal

--- add_comment.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-request_url = "http://192.168.0.248:8000"
-
-sub = "digitalcodesell"
-ids = ["fc5x9pe", "fc5pmpy", "fa8d7ys"]
-
-for id in ids:
-	print(requests.post(request_url + "/add-comment/", {"sub_name": sub, "comment_id": id}))
