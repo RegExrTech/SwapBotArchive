17210a6d795f374cfeb6e0c06a0bc0b2190b719d
Devon Wasson <devonwasson@gmail.com>
2020-07-14 13:33:09 +0000

    fixed copy user to take in both user names as params

--- tools/copy_user.py
+++ tools/copy_user.py
@@ -7,12 +7,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('sub_name', metavar='C', type=str)
-parser.add_argument('username', metavar='C', type=str)
-#parser.add_argument('swap_text', metavar='C', type=str)
+parser.add_argument('old_username', metavar='C', type=str)
+parser.add_argument('new_username', metavar='C', type=str)
 args = parser.parse_args()
 request_url = "http://192.168.0.248:8000"
-#requests.post(request_url + "/add-swap/", {'sub_name': args.sub_name, 'username': args.username, 'V': args.swap_text})
 json_helper = JsonHelper()
 db = json_helper.get_db('database/swaps.json')
-for swap in db[args.sub_name.lower()]['littlejohn04']:
-	requests.post(request_url + "/add-swap/", {'sub_name': args.sub_name.lower(), 'username': args.username.lower(), 'swap_text': swap})
+for swap in db[args.sub_name.lower()][args.old_username.lower()]:
+	requests.post(request_url + "/add-swap/", {'sub_name': args.sub_name.lower(), 'username': args.new_username.lower(), 'swap_text': swap})
