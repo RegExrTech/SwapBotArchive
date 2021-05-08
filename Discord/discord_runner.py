e1f9819a2de1ffd8813235098db8d4082de248d8
Devon Wasson <devonwasson@gmail.com>
2021-05-08 08:35:22 +0000

    added a discord folder for pairing discord accounts to reddit accounts

---
+++ Discord/discord_runner.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def main():
+	while True:
+		os.system('python swap.py')
+		time.sleep(10)
+
+time.sleep(3)
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ discord_runner.py').read().splitlines() if 'grep' not in x]
+# If the only output we get from grep is the grep itself and this instance of the runner,
+# then runner is not currently running so this instance should take over
+if len(ps_output) == 1:
+	main()
+elif len(ps_output) == 3:
+	os.system("kill $(ps aux | grep '[p]ython discord_runner.py' | awk '{print $2}')")
