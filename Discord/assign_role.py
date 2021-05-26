f03bdffef7bf80a6e4f7f1bbdbce388fc8e196d4
Devon Wasson <devonwasson@gmail.com>
2021-05-26 12:47:37 +0000

    swap.py now assigns discord roles, if applicable

--- Discord/assign_role.py
+++ Discord/assign_role.py
@@ -10,9 +10,4 @@ headers = {"Authorization":"Bot {}".format(TOKENS["token"]),
 roleURL = "https://discordapp.com/api/guilds/{}/members/{}/roles/{}"
 
 def assign_role(server_id, discord_user_id, role_id):
-	print("Would have assigned discord role " + role_id + " to discord user " + discord_user_id + " on server " + server_id)
-	return
 	requests.put(roleURL.format(server_id, discord_user_id, role_id), headers=headers)
-
-
- # 691788886367535224
