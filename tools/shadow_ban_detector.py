972ec6ae705eb783030e3c014db88c3a3f4c30e2
Devon Wasson <devonwasson@gmail.com>
2021-07-19 16:22:30 +0000

    added tool for checking if any bot accounts are shadowbanned

---
+++ tools/shadow_ban_detector.py
@@ -0,0 +1,18 @@
+import praw
+import sys
+sys.path.insert(0, '.')
+import config
+import os
+
+sub_config = config.Config('funkoppopmod')
+
+reddit = praw.Reddit(client_id=sub_config.client_id, client_secret=sub_config.client_secret, user_agent='Swap Bot for ' + sub_config.subreddit_name + ' v1.0 (by u/RegExr)', username=sub_config.bot_username, password=sub_config.bot_password)
+
+subnames = [x.split("-")[0] for x in os.listdir("config/")]
+for subname in subnames:
+	sub_config = config.Config(subname)
+	try:
+		bot = reddit.redditor(sub_config.bot_username)
+		m = bot.is_mod
+	except Exception as e:
+		print(sub_config.bot_username + " is shadowbanned.")
