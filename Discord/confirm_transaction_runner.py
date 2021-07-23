ac21ff2d0e36a2444fb6d30303d01cc8167c31b8
Devon Wasson <devonwasson@gmail.com>
2021-07-23 04:19:51 +0000

    modified runner for pair_discord to work correctly. Added runner for confirm_transaction.py

--- Discord/confirm_transaction_runner.py
+++ Discord/confirm_transaction_runner.py
@@ -3,15 +3,14 @@ import os
 
 def main():
 	while True:
-		os.system('python Discord/pair_discord.py')
+		os.system('python Discord/confirm_transaction.py')
 		time.sleep(10)
 
 time.sleep(3)
-ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ Discord/pair_discord_runner.py').read().splitlines() if 'grep' not in x]
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ Discord/confirm_transaction_runner.py').read().splitlines() if 'grep' not in x]
 # If the only output we get from grep is the grep itself and this instance of the runner,
 # then runner is not currently running so this instance should take over
-print(ps_output)
 if len(ps_output) == 1:
 	main()
 elif len(ps_output) == 3:
-	os.system("kill $(ps aux | grep '[p]ython Discord/pair_discord_runner.py' | awk '{print $2}')")
+	os.system("kill $(ps aux | grep '[p]ython Discord/confirm_transaction_runner.py' | awk '{print $2}')")
