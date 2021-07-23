5347215023d46f0559ee47fc3cc66a21b30cd43d
Devon Wasson <devonwasson@gmail.com>
2021-07-23 04:12:39 +0000

    DiscordConfig now expects to be called from outside the discord directory. also removed redundant configs

--- Discord/DiscordConfig.py
+++ Discord/DiscordConfig.py
@@ -3,7 +3,7 @@ import json_helper
 class Config():
 
 	def __init__(self, sub_name):
-		data = json_helper.get_db("config/"+sub_name+".json")
+		data = json_helper.get_db("Discord/config/"+sub_name+".json")
 		self.token = data["token"]
 		self.pairing_channel = data["pairing_channel"]
 		self.confirmation_channel = data["confirmation_channel"]
@@ -12,7 +12,5 @@ class Config():
 		self.server_id = data["server_id"]
 		self.bot_id = data["bot_id"]
 		self.bst_channels = data["bst_channels"]
-		self.subreddit_name = data["subreddit_name"]
-		self.database_name = self.subreddit_name.lower()
 		self.reddit_pairing_config = data["reddit_pairing_config"]
 		self.reddit_sub_config = data["reddit_sub_config"]
