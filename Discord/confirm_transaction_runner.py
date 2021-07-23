defc00c8dcb7e9d896984d3aa9cd96af5f5331e7
Devon Wasson <devonwasson@gmail.com>
2021-07-23 04:34:50 +0000

    confirm_transaction_runner.py now takes the sub name as input and passes it to confirm_transaction.py

--- Discord/confirm_transaction_runner.py
+++ Discord/confirm_transaction_runner.py
@@ -1,9 +1,15 @@
 import time
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('subreddit_name', metavar='C', type=str)
+args = parser.parse_args()
+subreddit_name = args.subreddit_name.lower()
 
 def main():
 	while True:
-		os.system('python Discord/confirm_transaction.py')
+		os.system('python Discord/confirm_transaction.py ' + subreddit_name)
 		time.sleep(10)
 
 time.sleep(3)
