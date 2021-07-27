a55fa7048af27726c090598d9426926d1fdfbac6
Devon Wasson <devonwasson@gmail.com>
2021-07-27 14:26:03 +0000

    remove_username_pairing.py now correctly sends data rather than json. Also, username2 is now correctly taken in as a string rather than an int

--- tools/remove_username_pairing.py
+++ tools/remove_username_pairing.py
@@ -9,10 +9,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('platform1', metavar='C', type=str)
 parser.add_argument('username1', metavar='C', type=str)
 parser.add_argument('platform2', metavar='C', type=str)
-parser.add_argument('username2', metavar='C', type=int)
+parser.add_argument('username2', metavar='C', type=str)
 args = parser.parse_args()
 request_url = "http://0.0.0.0:8000"
 json_helper = JsonHelper()
-removed = requests.post(request_url + "/remove_username_pairing/", json={'platform1': args.platform1.lower(), 'platform2': args.platform2.lower(), 'username1': args.username1.lower(), 'username2': args.username2.lower()}).json()
+removed = requests.post(request_url + "/remove-username-pairing/", data={'platform1': args.platform1.lower(), 'platform2': args.platform2.lower(), 'username1': args.username1.lower(), 'username2': args.username2.lower()}).json()
 for platform in removed:
 	print("Removed " + removed[platform] + " from " + platform)
