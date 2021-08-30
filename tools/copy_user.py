14a727aefb307565ce8e062541af25d0b33a45da
Devon Wasson <devonwasson@gmail.com>
2021-08-30 20:01:26 +0000

    copy user now correctly assigns flair to new username

--- tools/copy_user.py
+++ tools/copy_user.py
@@ -20,5 +20,5 @@ swap_text = ",".join(db[args.sub_name.lower()][args.platform.lower()][args.old_u
 requests.post(request_url + "/add-batch-swap/", json={'sub_name': args.sub_name.lower(), 'platform': args.platform.lower(), 'user_data': {args.new_username.lower(): swap_text}})
 
 sub_config, reddit, sub = swap.create_reddit_and_sub(args.sub_name.lower())
-swap_count = str(swap.get_swap_count(args.username.lower(), sub_config.gets_flair_from+[sub_config.database_name], args.platform.lower()))
+swap_count = str(swap.get_swap_count(args.new_username.lower(), sub_config.gets_flair_from+[sub_config.database_name], args.platform.lower()))
 swap.update_single_user_flair(sub, sub_config, args.new_username.lower(), swap_count, [], 0)
