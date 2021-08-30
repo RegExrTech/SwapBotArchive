e7463920234ec2b73692764adeb081feb67ef02b
Devon Wasson <devonwasson@gmail.com>
2021-08-30 18:59:08 +0000

    pair_discord_runner is now server-specific

--- Discord/pair_discord_runner.py
+++ Discord/pair_discord_runner.py
@@ -1,13 +1,18 @@
 import time
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('sub_name', metavar='C', type=str)
+args = parser.parse_args()
 
 def main():
 	while True:
-		os.system('python Discord/pair_discord.py')
+		os.system('python Discord/pair_discord.py ' + args.sub_name)
 		time.sleep(10)
 
 time.sleep(3)
-ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ Discord/pair_discord_runner.py').read().splitlines() if 'grep' not in x]
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ Discord/pair_discord_runner.py\ ' + args.sub_name).read().splitlines() if 'grep' not in x]
 # If the only output we get from grep is the grep itself and this instance of the runner,
 # then runner is not currently running so this instance should take over
 if len(ps_output) == 1:
