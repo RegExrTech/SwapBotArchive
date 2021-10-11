13eb09913ae514ece0d526798c9991c33712c20e
Devon Wasson <devonwasson@gmail.com>
2021-10-11 21:09:58 +0000

    copy_user.py now handles the cases where sub and platform do not exist in the db

--- tools/copy_user.py
+++ tools/copy_user.py
@@ -18,16 +18,18 @@ json_helper = JsonHelper()
 
 def main():
 	db = json_helper.get_db('database/swaps.json')
-	try:
-		swap_text = ",".join(db[args.sub_name.lower()][args.platform.lower()][args.old_username.lower()])
-	except:
+	if args.sub_name.lower() not in db:
+		print(args.sub_name.lower() + " was not found as a community in the database.")
+	elif args.platform.lower() not in db[args.sub_name.lower()]:
+		print(args.platform.lower() + " was not found as a platform in the " + args.sub_name.lower() + " community.")
+	elif args.old_username.lower() not in db[args.sub_name.lower()][args.platform.lower()]:
 		print(args.old_username.lower() + " was not found in the " + args.platform.lower() + " platform of the " + args.sub_name.lower() + " community. As such, there is nothing to copy over.")
-		return
-	requests.post(request_url + "/add-batch-swap/", json={'sub_name': args.sub_name.lower(), 'platform': args.platform.lower(), 'user_data': {args.new_username.lower(): swap_text}})
-
-	sub_config, reddit, sub = swap.create_reddit_and_sub(args.sub_name.lower())
-	swap_count = str(swap.get_swap_count(args.new_username.lower(), sub_config.gets_flair_from+[sub_config.database_name], args.platform.lower()))
-	swap.update_single_user_flair(sub, sub_config, args.new_username.lower(), swap_count, [], 0)
+	else:
+		swap_text = ",".join(db[args.sub_name.lower()][args.platform.lower()][args.old_username.lower()])
+		requests.post(request_url + "/add-batch-swap/", json={'sub_name': args.sub_name.lower(), 'platform': args.platform.lower(), 'user_data': {args.new_username.lower(): swap_text}})
+		sub_config, reddit, sub = swap.create_reddit_and_sub(args.sub_name.lower())
+		swap_count = str(swap.get_swap_count(args.new_username.lower(), sub_config.gets_flair_from+[sub_config.database_name], args.platform.lower()))
+		swap.update_single_user_flair(sub, sub_config, args.new_username.lower(), swap_count, [], 0)
 
 if __name__ == "__main__":
 	main()
