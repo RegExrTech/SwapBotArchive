aa3b88060480ba1cb1bd15c70ddd931100588c29
Devon Wasson <devonwasson@gmail.com>
2021-10-24 16:13:58 +0000

    removed dead variale in add batch swap tool

--- tools/add_batch_swap.py
+++ tools/add_batch_swap.py
@@ -15,5 +15,4 @@ request_url = "http://0.0.0.0:8000"
 json_helper = JsonHelper()
 requests.post(request_url + "/add-batch-swap/", json={'sub_name': args.sub_name.lower(), 'platform': args.platform.lower(), 'user_data': {args.username.lower(): ",".join(["LEGACY TRADE" for i in range(args.swap_count)])}})
 sub_config, reddit, sub = swap.create_reddit_and_sub(args.sub_name.lower())
-swap_count = str(swap.get_swap_count(args.username.lower(), sub_config.gets_flair_from+[sub_config.database_name], args.platform.lower()))
 swap.update_flair(reddit.redditor(args.username.lower()), None, sub_config)
