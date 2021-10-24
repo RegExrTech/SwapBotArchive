e067050913ad94f52c6517f8d16f6d5320b62eda
Devon Wasson <devonwasson@gmail.com>
2021-10-24 16:14:20 +0000

    copy user now updates flair in all relevant subs

--- tools/copy_user.py
+++ tools/copy_user.py
@@ -28,8 +28,7 @@ def main():
 		swap_text = ",".join(db[args.sub_name.lower()][args.platform.lower()][args.old_username.lower()])
 		requests.post(request_url + "/add-batch-swap/", json={'sub_name': args.sub_name.lower(), 'platform': args.platform.lower(), 'user_data': {args.new_username.lower(): swap_text}})
 		sub_config, reddit, sub = swap.create_reddit_and_sub(args.sub_name.lower())
-		swap_count = str(swap.get_swap_count(args.new_username.lower(), sub_config.gets_flair_from+[sub_config.database_name], args.platform.lower()))
-		swap.update_single_user_flair(sub, sub_config, args.new_username.lower(), swap_count, [], 0)
+		swap.update_flair(reddit.redditor(args.new_username.lower()), None, sub_config)
 
 if __name__ == "__main__":
 	main()
