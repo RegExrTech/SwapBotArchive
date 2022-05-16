f2ea99f53557a8d00d2923c8f70afd6b60efd4ce
Devon Wasson <devonwasson@gmail.com>
2022-05-16 00:09:19 +0000

    runner now no longer fails to run a sub if their sub name is the start of another sub name

--- runner.py
+++ runner.py
@@ -13,10 +13,10 @@ def main():
 		time.sleep(30)
 
 time.sleep(3)
-ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ runner.py\ ' + subreddit_name).read().splitlines() if 'grep' not in x]
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ runner.py\ ' + subreddit_name + "\ ").read().splitlines() if 'grep' not in x]
 # If the only output we get from grep is the grep itself and this instance of the runner,
 # then runner is not currently running so this instance should take over
 if len(ps_output) == 1:
 	main()
-elif len(ps_output) == 3:
-	os.system("kill $(ps aux | grep '[p]ython runner.py' | awk '{print $2}')")
+#elif len(ps_output) == 3:
+#	os.system("kill $(ps aux | grep '[p]ython runner.py' | awk '{print $2}')")
