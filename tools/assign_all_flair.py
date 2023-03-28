ddc0193f2a93f3d12b5fca3e6ea9ba14ab97125e
Devon Wasson <devonwasson@gmail.com>
2023-03-28 02:24:18 +0000

    better debuggubg for assign all flair tool

--- tools/assign_all_flair.py
+++ tools/assign_all_flair.py
@@ -26,32 +26,38 @@ if platform not in db[sub_config.subreddit_name]:
 unassigned_users = []
 keys = db[sub_config.subreddit_name][platform].keys()
 mods = [str(x).lower() for x in sub.moderator()]
+keys = mods
+
+keys = ['dimitritelep2113']
 
 for i in range(len(keys)):
 	user = keys[i].lower()
 	if user not in db[sub_config.subreddit_name][platform]:
 		db[sub_config.subreddit_name][platform][user] = []
-	count = str(get_swap_count(user, [sub_config.subreddit_name] + sub_config.gets_flair_from, platform))
+#	count = str(get_swap_count(user, [sub_config.subreddit_name] + sub_config.gets_flair_from, platform))
 	try:
-		print(str(i) + ") Updating user " + user + " to flair " + count)
+		print(str(i) + ") Updating user " + user)
 	except Exception as e:
 		continue
 	try:
 		redditor = reddit.redditor(user)
 		age = datetime.timedelta(seconds=(time.time() - redditor.created_utc)).days / 365.0
-		update_single_user_flair(sub, sub_config, redditor, count, unassigned_users, age)
+		update_flair(redditor, None, sub_config)
+#		update_single_user_flair(sub, sub_config, str(redditor), count, unassigned_users, age)
 		time.sleep(0.5)
 	except:
 		time.sleep(20)
 		try:
 			redditor = reddit.redditor(user)
 			age = datetime.timedelta(seconds=(time.time() - redditor.created_utc)).days / 365.0
-			update_single_user_flair(sub, sub_config, redditor, count, unassigned_users, age)
+			update_flair(redditor, None, sub_config)
+#			update_single_user_flair(sub, sub_config, str(redditor), count, unassigned_users, age)
 			time.sleep(0.5)
-		except:
-			print("    Unable to update flair for " + user)
+		except Exception as e:
+			print("    Unable to update flair for " + user + " with error " + str(e))
 			unassigned_users.append(user)
 
 
 if unassigned_users:
 	print("The following users did not get their flair updated:\n  " + "\n  ".join(unassigned_users))
+
