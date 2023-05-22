b0a0551b1ef23e2c36580c9e9fcb87d92c8eb007
Devon Wasson <devonwasson@gmail.com>
2023-05-22 13:28:11 +0000

    copy_user tool now has an 'all' option

--- tools/copy_user.py
+++ tools/copy_user.py
@@ -16,17 +16,26 @@ request_url = "http://0.0.0.0:8000"
 
 def main():
 	db = requests.get(request_url+"/get-db/").json()
-	if args.sub_name.lower() not in db:
-		print(args.sub_name.lower() + " was not found as a community in the database.")
-	elif args.platform.lower() not in db[args.sub_name.lower()]:
-		print(args.platform.lower() + " was not found as a platform in the " + args.sub_name.lower() + " community.")
-	elif args.old_username.lower() not in db[args.sub_name.lower()][args.platform.lower()]:
-		print(args.old_username.lower() + " was not found in the " + args.platform.lower() + " platform of the " + args.sub_name.lower() + " community. As such, there is nothing to copy over.")
+	sub_name = args.sub_name.lower()
+	platform = args.platform.lower()
+	old_username = args.old_username.lower()
+	new_username = args.new_username.lower()
+	if sub_name == 'all':
+		subs = [sub_name for sub_name in db if 'reddit' in db[sub_name] and old_username in db[sub_name]['reddit']]
 	else:
-		swap_text = ",".join(db[args.sub_name.lower()][args.platform.lower()][args.old_username.lower()])
-		requests.post(request_url + "/add-batch-swap/", json={'sub_name': args.sub_name.lower(), 'platform': args.platform.lower(), 'user_data': {args.new_username.lower(): swap_text}})
-		sub_config, reddit, sub = swap.create_reddit_and_sub(args.sub_name.lower())
-		swap.update_flair(reddit.redditor(args.new_username.lower()), None, sub_config)
+		subs = [sub_name]
+	for sub_name in subs:
+		if sub_name not in db:
+			print(sub_name + " was not found as a community in the database.")
+		elif platform not in db[sub_name]:
+			print(platform + " was not found as a platform in the " + sub_name + " community.")
+		elif old_username not in db[sub_name][platform]:
+			print(old_username + " was not found in the " + platform + " platform of the " + sub_name + " community. As such, there is nothing to copy over.")
+		else:
+			swap_text = ",".join(db[sub_name][platform][old_username])
+			requests.post(request_url + "/add-batch-swap/", json={'sub_name': sub_name, 'platform': platform, 'user_data': {new_username: swap_text}})
+			sub_config, reddit, sub = swap.create_reddit_and_sub(sub_name)
+			swap.update_flair(reddit.redditor(new_username), None, sub_config)
 
 if __name__ == "__main__":
 	main()
