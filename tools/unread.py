baf773578e01403b980cf0e8aa927ccaaf99d5a4
Devon Wasson <devonwasson@gmail.com>
2024-01-03 17:18:35 +0000

    unread.py now works on a time basis

--- tools/unread.py
+++ tools/unread.py
@@ -1,17 +1,19 @@
-#TODO only go back to the last hour, rather than an arbitrary number of comments (i.e.50)
 import praw
-import argparse
+import sys
 sys.path.insert(0, '.')
 import Config
 import os
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('config_file_name', metavar='C', type=str)
-#args = parser.parse_args()
-#config_fname = 'config/' + args.config_file_name
 subnames = [x.split("-")[0] for x in os.listdir("config/")]
 for subname in subnames:
+	print("=== " + subname + " ===")
 	sub_config = Config.Config(subname.split(".")[0])
+	if not sub_config.bot_username:
+		print("skipping...")
+		continue
 	reddit = sub_config.reddit_object
-	for message in reddit.inbox.all(limit=50):
+	for message in reddit.inbox.all(limit=None):
+		if message.created_utc < 1704247245:
+			break
 		message.mark_unread()
+		print("marking unread...")
