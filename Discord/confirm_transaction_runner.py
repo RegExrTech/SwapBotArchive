6eaa7cddfda3d74bef2c88f0e4d7c3f0fad3e4fb
Devon Wasson <devonwasson@gmail.com>
2024-01-03 21:31:12 +0000

    runners now call discord scripts with python3

--- Discord/confirm_transaction_runner.py
+++ Discord/confirm_transaction_runner.py
@@ -9,14 +9,14 @@ subreddit_name = args.subreddit_name.lower()
 
 def main():
 	while True:
-		os.system('python Discord/confirm_transaction.py ' + subreddit_name)
+		os.system('python3 Discord/confirm_transaction.py ' + subreddit_name)
 		time.sleep(10)
 
 time.sleep(3)
-ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ Discord/confirm_transaction_runner.py\ ' + subreddit_name).read().splitlines() if 'grep' not in x]
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python3\ Discord/confirm_transaction_runner.py\ ' + subreddit_name).read().splitlines() if 'grep' not in x]
 # If the only output we get from grep is the grep itself and this instance of the runner,
 # then runner is not currently running so this instance should take over
 if len(ps_output) == 1:
 	main()
 elif len(ps_output) == 3:
-	os.system("kill $(ps aux | grep '[p]ython Discord/confirm_transaction_runner.py' | awk '{print $2}')")
+	os.system("kill $(ps aux | grep '[p]ython3 Discord/confirm_transaction_runner.py' | awk '{print $2}')")
--- Discord/pair_discord_runner.py
+++ Discord/pair_discord_runner.py
@@ -8,14 +8,14 @@ args = parser.parse_args()
 
 def main():
 	while True:
-		os.system('python Discord/pair_discord.py ' + args.sub_name)
+		os.system('python3 Discord/pair_discord.py ' + args.sub_name)
 		time.sleep(10)
 
 time.sleep(3)
-ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python\ Discord/pair_discord_runner.py\ ' + args.sub_name).read().splitlines() if 'grep' not in x]
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python3\ Discord/pair_discord_runner.py\ ' + args.sub_name).read().splitlines() if 'grep' not in x]
 # If the only output we get from grep is the grep itself and this instance of the runner,
 # then runner is not currently running so this instance should take over
 if len(ps_output) == 1:
 	main()
 elif len(ps_output) == 3:
-	os.system("kill $(ps aux | grep '[p]ython Discord/pair_discord_runner.py' | awk '{print $2}')")
+	os.system("kill $(ps aux | grep '[p]ython3 Discord/pair_discord_runner.py' | awk '{print $2}')")
