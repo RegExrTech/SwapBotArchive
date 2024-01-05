504ce8eeebccbf0a7393604a1f434ae5ee32d818
Devon Wasson <devonwasson@gmail.com>
2024-01-05 23:25:52 +0000

    record_subs now only records subs that are actually active

--- tools/record_subs.py
+++ tools/record_subs.py
@@ -1,14 +1,18 @@
 import os
+import sys
+sys.path.insert(0, '.')
+import Config
 
 snames = []
 
-f = open("database/subreddits.txt", "r"))
+f = open("database/subreddits.txt", "r")
 old_content = f.read()
 f.close()
 
 for fname in os.listdir("config"):
-	sname = "r/" + fname.split("-")[0]
-	snames.append(sname)
+	raw_data = Config.get_json_data('config/' + fname)
+	if raw_data['bot_username'] and not raw_data['disabled']:
+		snames.append("r/" + fname.split(".")[0])
 
 snames.sort()
 new_content = "\n".join(snames)+"\n"
