b5740e995cdece763726896d23ef7109f290695f
Devon Wasson <devonwasson@gmail.com>
2024-03-22 17:42:02 +0000

    update server call for getting db when updating all flair

--- tools/assign_all_flair.py
+++ tools/assign_all_flair.py
@@ -16,40 +16,40 @@ platform = 'reddit'
 sub_config, reddit, sub = create_reddit_and_sub(args.sub_name.lower())
 
 request_url = "http://0.0.0.0:8000"
-db = requests.get(request_url+"/get-db/", data={'sub': sub_config.subreddit_name}).json()
+db = requests.get(request_url+"/get-sub-db/", data={'sub': sub_config.subreddit_name}).json()
 
 if platform not in db:
 	db[platform] = {}
 
 unassigned_users = []
-keys = db[platform].keys()
+keys = list(db[platform].keys())
 mods = [str(x).lower() for x in sub.moderator()]
-keys = mods
-
-keys = ['hesogross']
+#keys = mods
+#keys = ['hesogross']
 
 for i in range(len(keys)):
 	user = keys[i].lower()
 	if user not in db[platform]:
 		db[platform][user] = []
-#	count = str(get_swap_count(user, [sub_config.subreddit_name] + sub_config.gets_flair_from, platform))
+	count = str(get_swap_count(user, [sub_config.subreddit_name] + sub_config.gets_flair_from, platform))
 	try:
 		print(str(i) + ") Updating user " + user)
 	except Exception as e:
+		print(e)
 		continue
 	try:
 		redditor = reddit.redditor(user)
 		age = datetime.timedelta(seconds=(time.time() - redditor.created_utc)).days / 365.0
-		update_flair(redditor, None, sub_config)
-#		update_single_user_flair(sub, sub_config, str(redditor), count, unassigned_users, age)
+#		update_flair(redditor, None, sub_config)
+		update_single_user_flair(sub, sub_config, str(redditor), count, unassigned_users, age)
 		time.sleep(0.5)
 	except:
 		time.sleep(20)
 		try:
 			redditor = reddit.redditor(user)
 			age = datetime.timedelta(seconds=(time.time() - redditor.created_utc)).days / 365.0
-			update_flair(redditor, None, sub_config)
-#			update_single_user_flair(sub, sub_config, str(redditor), count, unassigned_users, age)
+#			update_flair(redditor, None, sub_config)
+			update_single_user_flair(sub, sub_config, str(redditor), count, unassigned_users, age)
 			time.sleep(0.5)
 		except Exception as e:
 			print("    Unable to update flair for " + user + " with error " + str(e))
